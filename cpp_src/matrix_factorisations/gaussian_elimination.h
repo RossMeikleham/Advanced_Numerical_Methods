a37abd97ed70298d70aa2d3150f0575e9de71156
Ross Meikleham <RossMeikleham@hotmail.co.uk>
2016-01-15 01:37:53 +0000

    Update gaussian_elimination.h

--- cpp_src/matrix_factorisations/gaussian_elimination.h
+++ cpp_src/matrix_factorisations/gaussian_elimination.h
@@ -6,45 +6,6 @@
 #include <vector>
 #include <tuple>
 
-/*
-% Naive method of Gaussian Elimination of a Given Matrix
-function a = naive_gauss_elimination(a) 
-    m = size(a, 1);
-    n = size(a, 2);
-
-    for k=1:(m-1)
-        for j=(k+1):m
-            a(j,k) = a(j, k) / a(k, k);
-            a(j,k+1:m) = a(j,k+1:m) - (a(j,k) * a(k,k+1:m));
-        end
-    end
-end
-
-
-
-function a = back_substitution_gauss(a)
-    m = size(a, 1);
-    n = size(a, 2);   
- 
-    % Row operations on result
-    for k=1:m-1
-        for i=k+1:m
-            a(i, n) = a(i, n) - (a(i, k) * a(k, n));
-        end
-    end
-
-    % Back substitution
-	a(m, n) = a(m, n) / a(m, m);
-	for i=m-1:-1:1
-		s = a(i, n);
-		for j=i+1:m
-			s = s - (a(i, j) * a(j, n));
-		end
-		a(i, n) = s / a(i, i);
-	end 
-end
-*/
-
 
 template <class Type>
 matrix<Type> naive_gauss_elimination(matrix<Type> const ax) {
